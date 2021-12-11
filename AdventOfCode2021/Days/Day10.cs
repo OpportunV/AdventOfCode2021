fe10d5c6f810489d259df8648658f939faaae782
OpportunV <RsTGear@gmail.com>
2021-12-11 12:44:46 +0300

    days 10 - 11

--- AdventOfCode2021/Days/Day10.cs
+++ AdventOfCode2021/Days/Day10.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using AdventOfCode2021.Helpers;
 
 namespace AdventOfCode2021.Days
@@ -7,19 +9,100 @@ namespace AdventOfCode2021.Days
     {
         private static readonly string _inputPath = Path.Combine("input",
             $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
+
+        private static readonly Dictionary<char, char> _brackets = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' }
+        };
         
+        private static readonly Dictionary<char, int> _scoresCorrupted = new Dictionary<char, int>
+        {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 }
+        };
+        
+        private static readonly Dictionary<char, int> _scoresAutocomplete = new Dictionary<char, int>
+        {
+            { ')', 1 },
+            { ']', 2 },
+            { '}', 3 },
+            { '>', 4 }
+        };
+        
+        
+
         public static object Part1()
         {
             var lines = Helper.GetInput(_inputPath);
+            
 
-            return -1;
+            return lines.Sum(CheckCorrupted);
         }
         
         public static object Part2()
         {
             var lines = Helper.GetInput(_inputPath);
+            var incomplete = lines.Where(item => CheckCorrupted(item) == 0);
+            var sorted = incomplete.Select(Autocomplete).OrderBy(item => item).ToArray();
+            return sorted[sorted.Length / 2];
+        }
+
+        private static int CheckCorrupted(string line)
+        {
+            var stack = new Stack<char>();
+            foreach (var chr in line)
+            {
+                if (_brackets.ContainsKey(chr))
+                {
+                    stack.Push(chr);
+                }
+                else
+                {
+                    if (_brackets[stack.Peek()] == chr)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return _scoresCorrupted[chr];
+                    }
+                }
+            }
             
-            return -1;
+            return 0;
+        }
+
+        private static long Autocomplete(string line)
+        {
+            var stack = new Stack<char>();
+            foreach (var chr in line)
+            {
+                if (_brackets.ContainsKey(chr))
+                {
+                    stack.Push(chr);
+                }
+                else
+                {
+                    if (_brackets[stack.Peek()] == chr)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            long ans = 0;
+            while (stack.Count > 0)
+            {
+                ans *= 5;
+                ans += _scoresAutocomplete[_brackets[stack.Pop()]];
+            }
+
+            return ans;
         }
     }
 }
--- AdventOfCode2021/Days/Day11.cs
+++ AdventOfCode2021/Days/Day11.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using AdventOfCode2021.Helpers;
 
 namespace AdventOfCode2021.Days
@@ -7,19 +9,105 @@ namespace AdventOfCode2021.Days
     {
         private static readonly string _inputPath = Path.Combine("input",
             $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
-        
+
+        private static long _flashCounter;
+        private static int _width;
+        private static int _height;
+        private static HashSet<(int, int)> _flashedThisTurn = new HashSet<(int, int)>();
+
         public static object Part1()
         {
             var lines = Helper.GetInput(_inputPath);
+            _width = lines[0].Length;
+            _height = lines.Length;
+            var octopuses = new int[_height][];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                octopuses[i] = lines[i].Select(item => int.Parse(item.ToString())).ToArray();
+            }
 
-            return -1;
+            for (int i = 0; i < 100; i++)
+            {
+                octopuses = Simulate(octopuses);
+            }
+            
+            return _flashCounter;
         }
         
         public static object Part2()
         {
             var lines = Helper.GetInput(_inputPath);
+            var octopuses = new int[_height][];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                octopuses[i] = lines[i].Select(item => int.Parse(item.ToString())).ToArray();
+            }
+
+            var step = 0;
+            while (true)
+            {
+                _flashCounter = 0;
+                octopuses = Simulate(octopuses);
+                step += 1;
+                if (_flashCounter == 100)
+                {
+                    return step;
+                }
+            }
+
+        }
+
+        private static int[][] Simulate(int[][] octopuses)
+        {
+            var next = new int[_height][];
+            for (int i = 0; i < _height; i++)
+            {
+                next[i] = new int[_width];
+                for (int j = 0; j < _width; j++)
+                {
+                    next[i][j] = octopuses[i][j] + 1;
+                }
+            }
             
-            return -1;
+            for (int i = 0; i < _height; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    if (next[i][j] > 9)
+                    {
+                        DoFlashes(next, (i, j));
+                    }
+                }
+            }
+            
+            _flashedThisTurn.Clear();
+            return next;
+        }
+
+        private static void DoFlashes(int[][] octopuses, (int, int) pos)
+        {
+            var (i, j) = pos;
+            if (_flashedThisTurn.Contains((i, j)))
+            {
+                return;
+            }
+            _flashedThisTurn.Add((i, j));
+            _flashCounter += 1;
+            octopuses[i][j] = 0;
+
+            foreach (var (x, y) in Helper.Adjacent((i, j), _width, _height))
+            {
+                if (_flashedThisTurn.Contains((x, y)))
+                {
+                    continue;
+                }
+                octopuses[x][y] += 1;
+                if (octopuses[x][y] > 9)
+                {
+                    DoFlashes(octopuses, (x, y));
+                }
+            }
+            
         }
     }
 }
